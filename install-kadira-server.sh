96d3940d9bf0862e48ae877f71c742adb76a68fc
Giordano Cardillo <giordano.cardillo@gmail.com>
2018-05-01 18:11:19 +0200

    Removed git from install script

--- install-kadira-server.sh
+++ install-kadira-server.sh
@@ -6,7 +6,7 @@ su - kadira
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get install -y git nano mongodb-org nodejs
+sudo apt-get install -y nano mongodb-org nodejs
 curl https://install.meteor.com/ | sh
 sudo systemctl enable mongod
 sudo systemctl start mongod
