bbcc4c70343565d3d31a00a7a36472ae707cfe19
Giordano Cardillo <giordano.cardillo@gmail.com>
2018-05-01 18:12:56 +0200

    Removed kadira user from install script

--- install-kadira-server.sh
+++ install-kadira-server.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-sudo adduser kadira
-sudo usermod -aG sudo kadira
-echo -e 'kadira ALL=(ALL) NOPASSWD:ALL' | sudo tee -a /etc/sudoers.d/90-cloud-init-users
-su - kadira
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
